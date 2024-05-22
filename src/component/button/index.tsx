7fe9e9c63522651437da96d79e929d20bfa5347d
yuva <yuva.prasath@flyerssoft.com>
2024-05-22 16:09:54 +0530

    Removed classnames package

--- src/component/button/index.tsx
+++ src/component/button/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { ButtonType } from "./type";
-import classNames from "classnames";
 import { Spinner } from "../spinner";
 
 export function Button(props: ButtonType) {
@@ -15,31 +14,30 @@ export function Button(props: ButtonType) {
     loaderClass = "",
   } = props;
 
-  const buttonClasses = classNames(
-    "relative px-4 py-2 flex items-center gap-1 rounded-lg font-Lato text-base font-medium ",
-    {
-      " bg-[#7700C7] hover:bg-[#8140ad] active:bg-[#9423de] text-white ":
-        variant === "primary",
-    },
-    {
-      "border-[#7700C7] border-solid border-[1px] bg-[#F4EDFF] text-[#7700C7] hover:bg-[#8140ad] hover:text-white active:bg-[#9423de] active:text-white":
-        variant === "secondary",
-    },
-    {
-      "disabled:bg-[#CDCDCD] disabled:cursor-not-allowed text-white opacity-50":
-        disabled,
-    },
-    className
-  );
+  const buttonClasses = () => {
+    switch (variant) {
+      case "primary":
+        return "border-0 bg-[#7700C7] hover:bg-[#8140ad] active:bg-[#9423de] text-white ";
+      case "secondary":
+        return "border-[#7700C7] border-solid border-[1px] bg-[#F4EDFF] !text-[#7700C7] hover:bg-[#8140ad] hover:text-white active:bg-[#9423de] active:text-white";
+      default:
+        break;
+    }
+  };
 
   return (
     <button
-      className={buttonClasses}
+      className={`${className} ${buttonClasses()} outline-none relative px-4 py-2 flex items-center gap-1 rounded-lg font-Lato text-base font-medium disabled:bg-[#CDCDCD] disabled:cursor-not-allowed text-white disabled:opacity-50`}
       type={type}
       disabled={disabled}
       onClick={onClick}
     >
-      {loading && <Spinner color="#7700C7" className={loaderClass} />}
+      {loading && (
+        <Spinner
+          color={variant === "primary" ? "white" : "#7700C7"}
+          className={loaderClass}
+        />
+      )}
       {children}
     </button>
   );
